057b60cfb5613d0a8223ca1ed16e8bbe889032b4
Yongzhang0916 <1803883132@qq.com>
2020-01-15 10:36:58 +0800

    branches detection

--- hackathon_vaa3d/DeepNeuron/blastneuron/seg_weight.h
+++
@@ -1,4 +0,0 @@
-#ifndef SEG_WEIGHT_H
-#define SEG_WEIGHT_H
-
-#endif // SEG_WEIGHT_H
--- hackathon_vaa3d/ImageProcessing/apo_to_markers.h
+++
@@ -1,8 +0,0 @@
-#ifndef APO_TO_MARKERS_H
-#define APO_TO_MARKERS_H
-#include "ImageProcessing_plugin.h"
-
-bool apo_to_markers(V3DPluginCallback2 &callback,const V3DPluginArgList &input,V3DPluginArgList &output,QWidget *parent,input_PARA &PARA);
-
-
-#endif // APO_TO_MARKERS_H
